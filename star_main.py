09d3f51e5113e3f632a6c10636b39f8118e51e61
mingtea <pitseleh79@gmail.com>
2017-09-20 22:36:30 -0400

    Separated system generating function

--- star_main.py
+++ star_main.py
@@ -1,34 +1,9 @@
 import random
+from gen_star import gen_star_sys
 
-print('Enter name of star system: ')
-star_sys_name = input()
-num_of_stars = 1
-
-star_type_int = random.randrange(0, 4)
-star_color = None
-star_type_dict = {
-	0: 'main sequence',
-	1: 'giant',
-	2: 'supergiant',
-	3: 'neutron',
-	4: 'black hole'}
-
-if star_type_int < 3:
-	star_color_int = random.randrange(0, 2)
-	star_color_dict = {
-	0: 'blue',
-	1: 'red',
-	2: 'white',
-	3: 'yellow'}
-	star_color = star_color_dict[star_color_int]
-elif star_type_int == 2:
-	star_color = 'red'
-elif star_type_int >= 3:
-	star_color = 'other'
-
-star_type = star_type_dict[star_type_int]
+def main():
+	gen_star_sys()
 
 
-print('The ' + star_sys_name + ' has ' + str(num_of_stars) + ' star')
-print(star_sys_name + ' is a ' + star_type + ' star.')
-print(star_sys_name + ' is a ' + star_color + ' star. ')
+if __name__ == '__main__':
+	main()
